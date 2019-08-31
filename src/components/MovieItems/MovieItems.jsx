066201c41a72f5b6538b1ff95f6f246647d0c9de
Max Maher <maxwmaher@gmail.com>
2019-08-31 13:05:13 -0500

    moved looping to movieitems file

--- src/components/MovieItems/MovieItems.jsx
+++ src/components/MovieItems/MovieItems.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import { Grid, Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+
 
 class MovieItems extends Component {
     render() {
         return (
-            <div>
-                <Grid item xs={4}>
-                    <Paper className={this.props.paper}>
-                    {this.props.movies.title}
-                    </Paper>
-                </Grid>
-            {/* <li key={this.props.movies.id}>{this.props.movies.title}</li> */}
-            </div>
+                <Grid item key={this.props.movies.title} align="center">
+                            <Card style={{ width: 500 }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            alt={this.props.movies.title}
+                            style={{ width: '35%', marginTop: 10 }}
+                            image={this.props.movies.poster}
+                            title={this.props.movies.title}
+                        />
+                        <CardContent>
+                            <Typography gutterbottom variant="h5" component="h2">
+                                {this.props.movies.title}
+                            </Typography>
+                            <Typography component="p">{this.props.movies.description}</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            Details
+                                    </Button>
+                    </CardActions>
+                </Card>
+                        </Grid>
         )
     }
 }
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import CenteredGrid from './index.jsx';
-// import MovieItems from '../MovieItems/MovieItems';
-import { Grid, Paper, Typography } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-
+import MovieItems from '../MovieItems/MovieItems';
+import { Grid } from "@material-ui/core";
 class MovieList extends Component {
 
     componentDidMount() {
@@ -26,46 +18,8 @@ class MovieList extends Component {
         return (
             <div style={{marginTop: 20, padding: 30}}>
                 <Grid container spacing={3} justify="center">
-                    {this.props.store.movies.map((movies) => (
-                        <Grid item key={movies.title} align="center">
-                            <Card style={{width: 500}}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        alt={movies.title}
-                                        style={{width: '35%', marginTop: 10}}
-                                        image={movies.poster}
-                                        title={movies.title}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterbottom variant="h5" component="h2">
-                                            {movies.title}
-                                        </Typography>
-                                        <Typography component="p">{movies.description}</Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                    <Button size="small" color="primary">
-                                        Details
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
+                    {this.props.store.movies.map((movies) => (<MovieItems movies={movies}/>))}
                 </Grid>
-
-
-
-                {/* This is the movie list!
-                <br /><br />
-                <Grid container spacing={3}>
-                {this.props.store.movies.map((movies) => {
-                    return (
-                        <MovieItems movies={movies} />
-                    )
-                })}
-                </Grid>
-                <CenteredGrid/> */}
             </div>
         )
     }
