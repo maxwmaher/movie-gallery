5bdc6f937049e1df2d6dd27f88ec567b6ff79a79
Max Maher <maxwmaher@gmail.com>
2019-08-31 12:53:45 -0500

    material-ui grid for movie display

--- src/components/MovieItems/MovieItems.jsx
+++ src/components/MovieItems/MovieItems.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 class MovieItems extends Component {
     render() {
         return (
             <div>
-            <li key={this.props.movies.id}>{this.props.movies.title}</li>
+                <Grid item xs={4}>
+                    <Paper className={this.props.paper}>
+                    {this.props.movies.title}
+                    </Paper>
+                </Grid>
+            {/* <li key={this.props.movies.id}>{this.props.movies.title}</li> */}
             </div>
         )
     }
 }
+
+
 export default connect()(MovieItems);
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import CenteredGrid from './index.jsx';
-import MovieItems from '../MovieItems/MovieItems'
+// import MovieItems from '../MovieItems/MovieItems';
+import { Grid, Paper, Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 
 class MovieList extends Component {
 
@@ -17,17 +24,48 @@ class MovieList extends Component {
 
     render() {
         return (
-            <div>
-                This is the movie list!
+            <div style={{marginTop: 20, padding: 30}}>
+                <Grid container spacing={3} justify="center">
+                    {this.props.store.movies.map((movies) => (
+                        <Grid item key={movies.title} align="center">
+                            <Card style={{width: 500}}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt={movies.title}
+                                        style={{width: '35%', marginTop: 10}}
+                                        image={movies.poster}
+                                        title={movies.title}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterbottom variant="h5" component="h2">
+                                            {movies.title}
+                                        </Typography>
+                                        <Typography component="p">{movies.description}</Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        Details
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+
+
+
+                {/* This is the movie list!
                 <br /><br />
-                <ul>
+                <Grid container spacing={3}>
                 {this.props.store.movies.map((movies) => {
                     return (
                         <MovieItems movies={movies} />
                     )
                 })}
-                </ul>
-                <CenteredGrid/>
+                </Grid>
+                <CenteredGrid/> */}
             </div>
         )
     }
